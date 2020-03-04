51a58e831b71e8fb73fef9976cca93f786399f49
ccrum292 <ccrum292@gmail.com>
2020-03-04 16:27:11 -0500

    finished ReadMe

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const path = require("path");
+var express = require("express");
+var path = require("path");
 const fs = require("fs");
+const data = require("./db/db.json")
 
-const app = express();
-let PORT = process.env.PORT || 3000;
+var app = express();
+var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -17,24 +19,48 @@ app.get("/notes", function(req, res){
 });
 
 app.get("/api/notes", function(req, res){
- 
- return  fs.readFile(__dirname + "/db/db.json", "utf8", function(err, data){
-    if (err){
-      throw err;
-    }
-    console.log(res.json(data));
-    return res.json(data);
-  })
+  res.json(data);
+  console.log(data);
 });
 
 app.post("/api/notes", function(req, res){
+  
+  let id;
+  if (!data.length) {
+    id = 0
+  }else {
+   id =  data[data.length-1].id +1
+  }
+  req.body.id = id;
+  data.push(req.body);
+
+  fs.writeFile((path.join(__dirname + "/db/db.json")), JSON.stringify(data), "utf8" , function(err){
+    if(err){
+      throw err;
+    }
+    res.json(data);
+    console.log("workin");
+  })
 
 });
 
-app.delete("/api/notes:id", function(req, res){
+app.delete("/api/notes/:id", function(req, res){
+ const id = req.params.id
+ console.log(id, '<======')
+ const theIndex = data.findIndex((note)=> note.id === parseInt(id))
+ console.log(theIndex)
+
+ data.splice(theIndex, 1);
+
+ fs.writeFile((path.join(__dirname + "/db/db.json")), JSON.stringify(data), "utf8" , function(err){
+  if(err){
+    throw err;
+  }
+  res.json(data);
+  console.log("workin");
+})
 
 });
-
 
 app.listen(PORT, function(){
   console.log("App listening on PORT "+ PORT);
